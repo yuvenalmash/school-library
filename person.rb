529b6f17e1ad99c8bff0270f6ce225cb044ecd17
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-03-09 01:33:56 +0300

    Create many to many rlship with rental at center

--- person.rb
+++ person.rb
@@ -2,7 +2,7 @@ require_relative 'nameable'
 
 class Person < Nameable
   attr_accessor :name, :age
-  attr_reader :id
+  attr_reader :id, :rentals
 
   def initialize(age, name = 'unknown', parent_permission: true)
     super()
@@ -10,6 +10,7 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   private
@@ -26,5 +27,10 @@ class Person < Nameable
 
   def correct_name
     @name
+  end
+
+  def add_rental(rental)
+    @rentals << rental
+    rental.person = self
   end
 end
