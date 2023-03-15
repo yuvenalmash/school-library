7d8e65441b2397117c0ab20cb73151f00ac21409
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-03-15 22:20:55 +0500

    Fix the linter

--- test_rental.rb
+++ test_rental.rb
@@ -5,20 +5,20 @@ require_relative './book'
 
 book = Book.new('The Hobbit', 'J.R.R. Tolkien')
 person = Student.new(18, 'Class A', 'John Doe')
-rental = Rental.new('2020-01-01' , book, person)
+rental = Rental.new('2020-01-01', book, person)
 puts rental.person.name
 
 describe 'Unit Test of rental' do
-    before(:each) do
-        @book = Book.new('The Hobbit', 'J.R.R. Tolkien')
-        @person = Student.new(18, 'Class A', 'John Doe')
-        @rental = Rental.new('2020-01-01' , @book, @person)
-    end
+  before(:each) do
+    @book = Book.new('The Hobbit', 'J.R.R. Tolkien')
+    @person = Student.new(18, 'Class A', 'John Doe')
+    @rental = Rental.new('2020-01-01', @book, @person)
+  end
 
-    it 'should return the date of the rental' do
-        expect(@rental.date).to eql('2020-01-01')
-    end
-    it 'check the be_instance_of rental' do
-        expect(@rental).to be_instance_of(Rental)
-    end
+  it 'should return the date of the rental' do
+    expect(@rental.date).to eql('2020-01-01')
+  end
+  it 'check the be_instance_of rental' do
+    expect(@rental).to be_instance_of(Rental)
+  end
 end
