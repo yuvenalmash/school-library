b70a976966ee5ffc78ea7ba1365fd0bea712c7ae
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-03-15 21:53:00 +0500

    Add a test of classroom

---
+++ test_classroom.rb
@@ -0,0 +1,14 @@
+require_relative './classroom'
+
+
+describe 'Unit Test of classroom' do
+  it 'should return the label of the classroom' do
+    classroom1 = Classroom.new('Class A')
+    expect(classroom1.label).to eql('Class A')
+  end
+
+  it 'should add a student to the classroom' do
+    classroom1 = Classroom.new('Class A')
+    expect(classroom1.add_student('student1')).to eql(['student1'])
+  end
+end
