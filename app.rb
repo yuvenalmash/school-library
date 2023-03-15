350754728d1e91ca56d674afe65491b7624d8787
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-03-16 00:25:59 +0300

    update main, app and file_management files

--- app.rb
+++ app.rb
@@ -1,83 +1,75 @@
+require_relative 'file_management'
+require_relative 'book'
 require_relative 'student'
 require_relative 'teacher'
-require_relative 'book'
 require_relative 'rental'
-require 'json'
 
 class App
-  attr_accessor :people, :books, :rentals
+  include FileManagment
+  attr_accessor :books, :people, :rentals
 
-  def initialize(people_raw, books_raw, rentals_raw)
-    @people = []
+  def initialize()
     @books = []
+    @people = []
     @rentals = []
-    convert_raw_data(people_raw, books_raw, rentals_raw)
   end
 
   def list_all_books
-    puts 'List of all books:'
+    puts 'List of books:'
     @books.each do |book|
       puts "\"#{book.title}\" by #{book.author}"
     end
   end
 
   def list_all_people
-    puts 'List of all people:'
+    puts 'List of people:'
     @people.each do |person|
       puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
     end
   end
 
   def create_person
-    print 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
-    input = gets.chomp
-
-    case input
-    when '1'
+    puts 'Do you want to create a student (1) or a teacher (2)? [Input the number]'
+    person_type = gets.chomp
+    if person_type == '1'
       create_student
-    when '2'
+    elsif person_type == '2'
       create_teacher
     else
-      puts 'Invalid input, please try again'
+      puts 'Invalid option, please try again'
     end
   end
 
   def create_student
     print 'Age: '
     age = gets.chomp
-
     print 'Name: '
     name = gets.chomp
-
     print 'Has parent permission? [Y/N]: '
     parent_permission = gets.chomp.upcase == 'Y'
     classroom = 'Class A'
     student = Student.new(age, classroom, name, parent_permission: parent_permission)
     @people << student
-    puts 'Student created successfully'
+    puts 'Person created successfully'
   end
 
   def create_teacher
     print 'Age: '
     age = gets.chomp
-
     print 'Name: '
     name = gets.chomp
-
     print 'Specialization: '
     specialization = gets.chomp
-    teacher = Teacher.new(age, specialization, name, parent_permission: true)
+    teacher = Teacher.new(age, specialization, name)
     @people << teacher
-    puts 'Teacher created successfully'
+    puts 'Person created successfully'
   end
 
   def create_book
     print 'Title: '
     title = gets.chomp
-
     print 'Author: '
     author = gets.chomp
-
     book = Book.new(title, author)
     @books << book
     puts 'Book created successfully'
@@ -86,72 +78,41 @@ class App
   def create_rental
     puts 'Select a book from the following list by number'
     @books.each_with_index do |book, index|
-      puts "#{index}) \"#{book.title}\" by #{book.author}"
+      puts "#{index}) Title: '#{book.title}', Author: #{book.author}"
     end
-
     book_index = gets.chomp.to_i
-
     puts 'Select a person from the following list by number (not id)'
     @people.each_with_index do |person, index|
       puts "#{index}) [#{person.class}] #{person.name}"
     end
-
     person_index = gets.chomp.to_i
-
     print 'Date: '
-    date = gets.chomp
-
+    date = gets.chomp.to_s
     rental = Rental.new(date, @books[book_index], @people[person_index])
     @rentals << rental
     puts 'Rental created successfully'
   end
 
-  def list_persons_rentals
+
+  def list_all_rentals_for_person_id
     list_all_people
     print 'ID of person: '
     id = gets.chomp.to_i
-
+    rentals = @rentals.filter { |rental| rental.person.id == id }
     puts 'Rentals:'
-    @rentals.each do |rental|
-      puts "Date: #{rental.date}, Book: \"#{rental.book.title}\" by #{rental.book.author}" if rental.person.id == id
+    rentals.each do |rental|
+      puts "Date: #{rental.date}, Book: '#{rental.book.title}' by #{rental.book.author}"
     end
   end
 
-  def convert_raw_data(people_raw, books_raw, rentals_raw)
-    convert_raw_people(people_raw)
-    convert_raw_books(books_raw)
-    convert_raw_rentals(rentals_raw)
+  def save_data
+    FileManagment.save_to_files(@books, @people, @rentals)
   end
 
-  def convert_raw_people(people_raw)
-    people_raw.each do |person|
-      person = JSON.parse(person)
-      @people << case person['type']
-                 when 'Student'
-                   Student.new(person['age'], person['classroom'], person['name'],
-                               parent_permission: person['parent_permission'])
-                 when 'Teacher'
-                   Teacher.new(person['age'], person['specialization'], person['name'], parent_permission: true)
-                 end
-    end
-  end
-
-  def convert_raw_books(books_raw)
-    books_raw.each do |book|
-      book = JSON.parse(book)
-      @books << Book.new(book['title'], book['author'])
-    end
-  end
-
-  def convert_raw_rentals(rentals_raw)
-    rentals_raw.each do |rental|
-      rental = JSON.parse(rental)
-      book = JSON.parse(rental['book'])
-      person = JSON.parse(rental['person'])
-      book = @books.find { |b| b.title == book['title'] && b.author == book['author'] }
-      person = @people.find { |p| p.name == person['name'] && p.age == person['age'] }
-
-      @rentals << Rental.new(rental['date'], book, person)
-    end
+  def load_data
+    data = FileManagment.load_from_files
+    @books = data[0]
+    @people = data[1]
+    @rentals = data[2]
   end
 end
--- file_management.rb
+++ file_management.rb
@@ -1,72 +1,113 @@
 require 'json'
-require_relative 'app'
+require_relative 'book'
+require_relative 'student'
+require_relative 'teacher'
+require_relative 'rental'
 
-class FileManagment
-  def read_file(file_name)
-    unless File.exist?(file_name)
-      File.new(file_name, 'w')
-      write_file(file_name, [])
+module FileManagment
+  def self.save_to_files(books, people, rentals)
+    save_books_to_files(books)
+    save_people_to_files(people)
+    save_rentals_to_files(rentals)
+  end
+
+  def self.save_books_to_files(books)
+    books_json = books.map do |book|
+      {
+        'title' => book.title,
+        'author' => book.author
+      }
     end
-
-    File.open(file_name, 'r') do |f|
-      json_string = f.read
-      JSON.parse(json_string)
-    end
+    File.write('storage_files/books.json', JSON.generate(books_json))
   end
 
-  def load_from_files
-    people = read_file('storage_files/people.json')
-    books = read_file('storage_files/books.json')
-    rentals = read_file('storage_files/rentals.json')
-    [people, books, rentals]
-  end
-
-  def write_file(file_name, data)
-    File.write(file_name, JSON.generate(data))
-  end
-
-  def save_to_files(books_list, people_list, rentals_list)
-    save_books_to_files(books_list)
-    save_people_to_files(people_list)
-    save_rentals_to_files(rentals_list)
-  end
-
-  def save_people_to_files(people_list)
-    people = []
-    people_list.each do |person|
-      if person.instance_of?(Student)
-        student_hash = { id: person.id, name: person.name, age: person.age,
-                         parent_permission: person.parent_permission, classroom: person.classroom, type: 'Student' }
-        people.push(JSON.generate(student_hash))
-      elsif person.instance_of?(Teacher)
-        teacher_hash = { id: person.id, name: person.name, age: person.age, specialization: person.specialization,
-                         type: 'Teacher' }
-        people.push(JSON.generate(teacher_hash))
+  def self.save_people_to_files(people)
+    people_json = people.map do |person|
+      if person.class == Student
+        {
+          'id' => person.id,
+          'name' => person.name,
+          'age' => person.age,
+          'classroom' => person.classroom,
+          'parent_permission' => person.parent_permission,
+          'type' => person.class.to_s
+        }
+      elsif person.class == Teacher
+        {
+          'id' => person.id,
+          'name' => person.name,
+          'age' => person.age,
+          'specialization' => person.specialization,
+          'parent_permission' => person.parent_permission,
+          'type' => person.class.to_s
+        }
       end
     end
-    write_file('storage_files/people.json', people)
+    File.write('storage_files/people.json', JSON.generate(people_json))
   end
 
-  def save_books_to_files(books_list)
-    books = []
-    books_list.each do |book|
-      book_hash = { title: book.title, author: book.author }
-      books.push(JSON.generate(book_hash))
+  def self.save_rentals_to_files(rentals)
+    rentals_json = rentals.map do |rental|
+      {
+        'date' => rental.date,
+        'book' => {
+          'title' => rental.book.title,
+          'author' => rental.book.author
+        },
+        'person' => {
+          'id' => rental.person.id,
+          'name' => rental.person.name,
+          'age' => rental.person.age
+        }
+      }
     end
-    write_file('storage_files/books.json', books)
+    File.write('storage_files/rentals.json', JSON.generate(rentals_json))
   end
 
-  def save_rentals_to_files(rentals_list)
-    rentals = []
-    rentals_list.each do |rental|
-      book_hash = { title: rental.book.title, author: rental.book.author }
-      book = JSON.generate(book_hash)
-      person_hash = { id: rental.person.id, name: rental.person.name, age: rental.person.age,
-                      rentals: [book_hash, rental.date] }
-      person = JSON.generate(person_hash)
-      rental_hash = { date: rental.date, book: book, person: person }
-      rentals.push(JSON.generate(rental_hash))
+  def self.load_from_files
+    books = load_books_from_files('storage_files/books.json')
+    people = load_people_from_files('storage_files/people.json')
+    rentals = load_rentals_from_files(people, books, 'storage_files/rentals.json')
+    [books, people, rentals]
+  end
+
+  def self.load_books_from_files(file_name)
+    if_file_exists?(file_name)
+    books_json = File.read(file_name)
+    books = JSON.parse(books_json)
+    books.map do |book|
+      Book.new(book['title'], book['author'])
     end
-    write_file('storage_files/rentals.json', rentals)
+  end
+
+  def self.load_people_from_files(file_name)
+    if_file_exists?(file_name)
+    people_json = File.read(file_name)
+    people = JSON.parse(people_json)
+    people.map do |person|
+      if person['type'] == 'Student'
+        Student.new(person['age'], person['classroom'], person['name'], parent_permission: person['parent_permission'])
+      elsif person['type'] == 'Teacher'
+        Teacher.new(person['age'], person['specialization'], person['name'])
+      end
+    end
+  end
+
+  def self.load_rentals_from_files(people,books,file_name)
+    if_file_exists?(file_name)
+    rentals_json = File.read(file_name)
+    rentals = JSON.parse(rentals_json)
+    rentals.map do |rental|
+      rental['person'] = people.find { |p| p.name == rental['person']['name'] && p.age == rental['person']['age'] }
+      rental['book'] = books.find { |b| b.title == rental['book']['title'] && b.author == rental['book']['author'] }
+      Rental.new(rental['date'], rental['book'], rental['person'])
+    end
+  end
+
+  def self.if_file_exists?(file_name)
+    unless File.exist?(file_name)
+      File.new(file_name, 'w')
+      File.write(file_name, [])
+    end
   end
 end
