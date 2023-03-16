7d7fed5a12faf501f151296ae5994ebdb810b26d
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-03-16 13:57:34 +0500

    Unit test of trimmer

--- spec/trimmer_decorator_spec.rb
+++ spec/trimmer_decorator_spec.rb
@@ -1,4 +1,6 @@
 require_relative '../trimmer_decorator'
+require_relative '../person'
+require_relative '../capitalize_decorator'
 
 describe 'Unit Test of trimmer_decorator' do
   before(:each) do
@@ -7,5 +9,11 @@ describe 'Unit Test of trimmer_decorator' do
 
   it 'should return NotImplementedError' do
     expect { @trimmer_decorator.correct_name }.to raise_error(NotImplementedError)
+  end
+
+  it 'should return the trim name' do
+  person = Person.new(25, 'John Doe')
+  trim = TrimmerDecorator.new(person)
+  expect(trim.correct_name).to eql('John Doe')
   end
 end
