f2a3821d4b7f00dda94723c72b0f9a48a0754887
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-03-16 06:45:22 +0500

    Erase useless code

--- test_rental.rb
+++ test_rental.rb
@@ -3,11 +3,6 @@ require_relative './student'
 require_relative './rental'
 require_relative './book'
 
-book = Book.new('The Hobbit', 'J.R.R. Tolkien')
-person = Student.new(18, 'Class A', 'John Doe')
-rental = Rental.new('2020-01-01', book, person)
-puts rental.person.name
-
 describe 'Unit Test of rental' do
   before(:each) do
     @book = Book.new('The Hobbit', 'J.R.R. Tolkien')
