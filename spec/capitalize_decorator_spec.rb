6f5ecf69dff7ab82e46a32cdec8b3e38b8962b08
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-03-16 13:58:17 +0500

    Unit test of capitalize

--- spec/capitalize_decorator_spec.rb
+++ spec/capitalize_decorator_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../capitalize_decorator'
+require_relative '../person'
 
 describe 'Unit Test of capitalize_decorator' do
   before(:each) do
@@ -7,5 +8,11 @@ describe 'Unit Test of capitalize_decorator' do
 
   it 'should return NotImplementedError' do
     expect { @capitalize_decorator.correct_name }.to raise_error(NotImplementedError)
+  end
+
+  it 'should return the capitalize name' do
+    person = Person.new(25, 'John Doe')
+    capitalize = CapitalizeDecorator.new(person)
+    expect(capitalize.correct_name).to eql('John doe')
   end
 end
